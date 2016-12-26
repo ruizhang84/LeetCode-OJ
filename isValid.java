11314f415ed1e0a0412e85c0c987d2770fe6bdb5
rzhang360 <rzhang360@gatech.edu>
2016-12-25 21:49:43 -0500

    valid parentheses :

--- isValid.java
+++ isValid.java
@@ -1,21 +1,25 @@
 public class Solution {
     public boolean isValid(String s) {
+        if(s.length()==0||s.length()==1)
+            return false;
+        
         Stack<Character> st = new Stack<Character>();
         
         for (int i = 0; i<s.length(); i++){
-            if (s.charAt(i)  == '[' || s.charAt(i) == '{' || s.charAt(i) == '('){
-                st.push(s.charAt(i));
+            char curr = s.charAt(i);
+            if (curr == '[' || curr  == '{' || curr  == '('){
+                st.push(curr);
                 
-            }else if (s.charAt(i) == ')'){
+            }else if (curr  == ')'){
                 
                 if (st.empty()|| st.pop() != '(')
                     return false;
                 
-            }else if (s.charAt(i) == ']'){
+            }else if (curr  == ']'){
                 if (st.empty()|| st.pop() != '[')
                     return false;
                 
-            }else if (s.charAt(i) == '}'){
+            }else if (curr  == '}'){
                 if (st.empty()|| st.pop() != '{')
                     return false;
                 
