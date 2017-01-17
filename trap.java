e57dd8fb89d64dd1c13a6d40e481dda0dd829ded
rzhang360 <rzhang360@gatech.edu>
2017-01-17 09:37:25 -0500

    Trapping Rain Water

---
+++ trap.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int trap(int[] height) {
+        int len = height.length;
+        if (len < 2)
+            return 0;
+        
+        int left = 0;
+        int right = len-1;
+        int total = 0;
+        boolean leftcount = false;
+        boolean rightcount = false;
+        int leftbound = height[left];
+        int rightbound = height[right];
+        
+        while (left < right){
+            //process left
+            while (leftbound <= rightbound && left < right){
+                if (height[left] <= leftbound){
+                    total += leftbound - height[left];
+                    left++;
+                }else{
+                    leftbound = height[left];
+                }
+            }
+            right--;
+            
+            while (leftbound > rightbound && left < right){
+                if (height[right] <= rightbound){
+                    total += rightbound - height[right];
+                    right--;
+                }else{
+                    rightbound = height[right];
+                }
+            }
+            left++;
+        }
+        
+        
+        return  total;
+        
+        
+    }
+}
