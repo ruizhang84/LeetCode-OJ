5f1f9165478f631df89369b8da097437064c319e
Rui <ruizhang84@users.noreply.github.com>
2017-03-03 13:46:13 -0500

    Update Word Ladder

--- ladderLength.py
+++ ladderLength.py
@@ -7,21 +7,39 @@ class Solution(object):
         :rtype: int
         """
         wordDict = dict.fromkeys(wordList, 0)
-        queue = [(beginWord, 1)]
+        if endWord not in wordDict:
+            return 0
+        queue_front = set([beginWord])
+        queue_back = set([endWord])
+        dist = 1
         
-        while queue:
-            s, level = queue.pop(0)
-            if s == endWord:
-                return level
-
-            for i in range(len(s)):
-                for c in 'abcdefghijklmnopqrstuvwxyz':
-                    if c != s[i]:
-                        w = s[:i] + c + s[i+1:]
-                        if w in wordDict:
-                            del wordDict[w]
-                            queue.append((w, level +1))
-        return 0  
-        
-        
-        
+        while queue_front and queue_back:
+            dist += 1
+            if len(queue_front) <= len(queue_back):
+                front = set([])
+                for s in queue_front:
+                    for i in range(len(s)):
+                        for c in 'abcdefghijklmnopqrstuvwxyz':
+                            if c != s[i]:
+                                w = s[:i] + c + s[i+1:]
+                                if w in queue_back:
+                                    return dist 
+                                if w in wordDict:
+                                    del wordDict[w]
+                                    front.add(w)
+                queue_front = front
+            else:
+                back = set([])
+                for s in queue_back:    
+                    for i in range(len(s)):
+                        for c in 'abcdefghijklmnopqrstuvwxyz':
+                            if c != s[i]:
+                                w = s[:i] + c + s[i+1:]
+                                if w in queue_front:
+                                    return dist 
+                                if w in wordDict:
+                                    del wordDict[w]
+                                    back.add(w)
+                queue_back = back
+            
+        return 0   
