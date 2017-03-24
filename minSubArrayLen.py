ae817e4d6ace79021e84819ea1ddd26a9b4a0024
Rui <ruizhang84@users.noreply.github.com>
2017-03-24 14:57:21 -0400

    inimum Size Subarray Sum

---
+++ minSubArrayLen.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = right = 0
+        total = 0
+        ans = len(nums)+1
+        while right < len(nums):
+            while total < s and right < len(nums):
+                total += nums[right]
+                right += 1
+            while total >= s:
+                ans = min(ans, right-left)
+                total -= nums[left]
+                left += 1
+
+        if ans == len(nums)+1 :
+            return 0
+        return ans
+        
+        
+        
+        
