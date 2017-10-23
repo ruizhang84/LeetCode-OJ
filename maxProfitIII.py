15836e5eb5ed28a424c417692268e4d7d8148e97
Rui <ruizhang84@users.noreply.github.com>
2017-10-23 12:01:08 -0400

    Update maxProfitIII.py

--- maxProfitIII.py
+++ maxProfitIII.py
@@ -9,32 +9,32 @@ class Solution(object):
         
         ans = 0
         best = [0] * len(prices)
-        minp1 = maxp1 = prices[0]
-        minp2 = maxp2 = prices[-1]
+        minv = prices[0]
+        maxv = prices[-1]
     
         temp_best = 0
         for i in range(len(prices)):
-            temp_best, minp1, maxp1 = maxProfitextend(temp_best, prices[i], minp1, maxp1, True)
+            temp_best, minv = maxProfitextend(temp_best, prices[i], minv, True)
             best[i] = temp_best
             
         temp_best = 0
         for i in range(len(prices)):
-            temp_best, minp2, maxp2 = maxProfitextend(temp_best, prices[-i-1], minp2, maxp2, False)
+            temp_best, maxv = maxProfitextend(temp_best, prices[-i-1], maxv, False)
             ans  = max(ans, best[-i-1] + temp_best)
         return ans
 
 
-def maxProfitextend(best, price_i, minp, maxp, to_right):
+def maxProfitextend(best, price_i, minimax, to_right):
     if to_right:
-        if price_i > minp:
-            best = max(best, price_i - minp)
-        if minp > price_i:
-            minp = price_i
-        return (best, minp, maxp)
+        if price_i > minimax:
+            best = max(best, price_i - minimax)
+        if minimax > price_i:
+            minimax = price_i
+        return (best, minimax)
     
-    if price_i < maxp:
-        best = max(best, maxp - price_i)
-    if maxp < price_i:
-        maxp = price_i
-    return (best, minp, maxp)
+    if price_i < minimax:
+        best = max(best, minimax - price_i)
+    if minimax < price_i:
+        minimax = price_i
+    return (best, minimax)
         
